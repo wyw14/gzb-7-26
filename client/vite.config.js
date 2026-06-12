62a58904a81e0d172a050941fad9b69f6f013ad0
PINRU Local <pinru@local>
2026-06-13 02:16:15 +0800

    gzb-7-26

---
+++ client/vite.config.js
@@ -0,0 +1,22 @@
+﻿import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    port: 5126,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3126',
+        changeOrigin: true
+      }
+    }
+  }
+})
+
